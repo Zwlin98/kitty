897ef708cb587263abbcad06bbdc2f51266e17f8
Zwlin <zwlin98@outlook.com>
2025-03-26 13:03:43 +0800

    feat: add kitty-scroll-back.nvim

--- auth.py
+++
@@ -1,6 +0,0 @@
-# https://sw.kovidgoyal.net/kitty/remote-control/#customizing-authorization-with-your-own-program
-def is_cmd_allowed(pcmd, window, from_socket, extra_data):
-    cmd_name = pcmd['cmd']
-    allowed_cmds = ['set-tab-title']
-    return cmd_name in allowed_cmds
-
